b10eb16057eda79ce87959d2cdb200f8d843f362
Emesiana Celestine <celelove4all@gmail.com>
2024-01-24 11:42:01 +0100

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++){
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  
+}
+*/
+
+//const gifts = ["teddy bear", "drone", "doll"];
+  
+
+function writeCards(gift, name) {
+
+ 
+   let card = [];
+
+  for (let i = 0; i < gift.length; i++) {
+    
+  
+    
+   card.push( `Thank you, ${gift[i]}, for the wonderful ${name} gift!`);
+
+    
+    
+
+  }
+
+  return card;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], " surprise ");
+
+
+
+
+function countDown(a){
+  while(a >= 0 ){
+    console.log(a)
+    --a
+  }
+}
